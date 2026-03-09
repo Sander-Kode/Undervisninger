ad51c6a8d846acd13c85d77a7e222873ea326d56
Sander <smk012026@gmail.com>
2026-03-09 13:42:56 +0100

    koden fungerer ikke, feilsøker

--- Undervisning 4 Javascript/app.js
+++ Undervisning 4 Javascript/app.js
@@ -1,7 +1,56 @@
 const myButton = document.getElementById("myButton");
+const myText = document.getElementById("myText");
+const fruitText = document.querySelectorAll
+const fruits = [
+    {
+        name: "Apple",
+        color: "green",
+        calories: 52
+    },
 
-function greeting(){
-    console.log("Hello")
+    {
+        name: "Orange",
+        color: "orange",
+        calories: 47 
+    }
+
+
+]
+
+const container = document.createElement("section");
+container.classList.add("fruit-list");
+document.body.appendChild(container);
+
+
+    
+    
+function renderFruits() {
+    for(let fruit of fruits) {
+        
+        const title = document.createElement("h3");
+        title.classList.add("name-title");
+
+        title.textContent = fruit.name;
+        
+        
+        document.body.appendChild(title);
+        
+
+        
+    }
 }
 
-myButton.addEventListener
+
+
+
+function greeting() {
+    myText.textContent = "Hello!";
+    
+}
+
+
+
+myButton.addEventListener("click", greeting);
+
+
+
