7f6ca5ec59373a41f81e32ca4ddbf6c7541eb467
Sander <smk012026@gmail.com>
2026-04-08 11:12:23 +0200

    undervisning dag 2 fullført

--- Undervisning 4 Javascript advanced/script.js
+++ Undervisning 4 Javascript advanced/script.js
@@ -1,9 +1,20 @@
 const taskForm = document.querySelector("#task-form");
 const taskInput = document.querySelector("#task-input");
 const listContainer = document.querySelector("#list-container");
+const showCompletedEl = document.querySelector("#show-completed")
 
 
 let tasks = [];
+let filters = {showCompleted: false};
+
+const saveTasksToStorage = () => localStorage.setItem('tasks', JSON.stringify(tasks))
+
+showCompletedEl.addEventListener('change', (e) => {
+
+    filters.showCompleted = e.target.checked;
+    renderPage();
+
+});
 
 taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -20,6 +31,8 @@ taskForm.addEventListener('submit', (e) => {
         complete: false
     })
 
+    saveTasksToStorage();
+
     renderPage();
 })
 
@@ -30,10 +43,49 @@ const completeTaskInput = (task) => {
 
     inputElement.addEventListener('change', (e) => {
         task.complete = e.target.checked;
+        saveTasksToStorage();
         renderPage();
     })
     
     return inputElement
+}
+
+const deleteTaskButton = (task) => {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', () => {
+        const taskIndex = tasks.indexOf(task);
+        if (taskIndex >= 0) {
+            tasks.splice(taskIndex, 1)
+        }
+        saveTasksToStorage();
+        renderPage();
+    })
+
+    return deleteBtn;
+}
+
+const editTaskButton = (task, taskName) => {
+    const editTaskBtn = document.createElement('button');
+    editTaskBtn.classList.add('edit-button');
+    editTaskBtn.textContent = 'edit'
+
+    editTaskBtn.addEventListener('click', () => {
+        taskName.readOnly = !taskName.readOnly;
+        editTaskBtn.textContent = taskName.readOnly ? "Edit" : "Save"; 
+        task.task = taskName.value;
+
+        saveTasksToStorage();
+
+
+
+    });
+
+
+
+    return editTaskBtn
 }
 
 
@@ -41,13 +93,19 @@ const buildPage = (tasksArr) => {
     listContainer.replaceChildren();
     tasksArr.forEach(task => {
         const taskContainer = document.createElement('div');
+        taskContainer.classList.add('task-container');
+        task.complete ? taskContainer.classList.add('completed') : taskContainer.classList.remove('completed');
 
-        const taskName = document.createElement('p');
-        taskName.textContent = task.task
+        const taskName = document.createElement('input');
+        taskName.classList.add('task-name');
+        taskName.readOnly = true;
+        taskName.value = task.task
 
         const completeInput = completeTaskInput((task))
+        const deleteTaskBtn = deleteTaskButton(task)
+        const editTaskBtn = editTaskButton(task, taskName)
 
-        taskContainer.append(taskName, completeInput);
+        taskContainer.append(taskName, completeInput, editTaskBtn, deleteTaskBtn);
 
 
         listContainer.append(taskContainer);
@@ -55,8 +113,16 @@ const buildPage = (tasksArr) => {
 };
 
 
+const filterArr = (tasksArr) => {
+    return tasksArr.filter(task => filters.showCompleted | !task.complete)
+}
+
 const renderPage = () => {
-    buildPage(tasks)
+    const storedTasks = localStorage.getItem('tasks')
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks)
+    }
+    buildPage(filterArr(tasks))
 };
 
 renderPage();
