4274538c53e8ac376736bc9a983ff53cf8bce079
Sander <smk012026@gmail.com>
2026-04-28 10:47:58 +0200

    esktra undervisning fullført, klasser

--- js advanced re 2/app.js
+++ js advanced re 2/app.js
@@ -36,3 +36,70 @@ Hvordan setter vi opp en event listener?
 - hva skal den kjøre () => {kodeblokk for hva den skal gjøre}
 
 */
+
+
+// Hva er en klasse, hvordan lager man en klasse?
+// Klasser bruker PascalCase, ikke camelCase.
+
+//Klasser bruker Constructor for å fortelle hva objektet er, navn, breed, alder etc
+
+class Cat {
+    constructor (name, breed, age, speak, hiss) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        
+    }
+    speak() {
+        console.log('MEEEOW')
+    }
+
+    hiss () {
+        console.log('HIIIISSSS')
+
+    }
+};
+
+const villemo = new Cat('Villemo', 'norsk skogskatt', 5);
+const findus = new Cat('Findus', 'blandingsrase', 10);
+
+console.log(villemo, findus)
+
+console.log('Both cats aged one year')
+villemo.age += 1;
+findus.age += 1;
+
+console.log('Villemos new age is', villemo.age)
+console.log('Findus new age is', findus.age)
+
+findus.name = "Ludo";
+
+console.log('Finus has gotten a new name, the name is', findus.name);
+
+
+class Entity {
+    constructor(name, hps, atk){
+        this.name = name;
+        this.hps = hps;
+        this.atk = atk;
+
+    }
+
+    fight (entity) {
+        console.log(`${this.name} hits ${entity.name} for ${this.atk}`);
+        entity.hps -= this.atk;
+        console.log(`${entity.name} now has ${entity.hps} health left`);
+    }
+
+}
+
+const hero = new Entity('Sander', 10, 5);
+const enemy = new Entity('Bob', 5, 3);
+
+hero.fight(enemy)
+hero.fight(hero)
+
+
+
+
+
